ef3b71ed34cc47a8751386b3f07b970c71d52a72
biggestbarnacle1 <52912384+biggestbarnacle1@users.noreply.github.com>
2020-02-24 03:57:50 -0700

    Update

--- uschool.ino
+++ uschool.ino
@@ -1,66 +1,53 @@
-#define Player1FirstPin 10
-#define Player1SecondPin 9
-#define MiddlePin 8
-#define Player2FirstPin 6
-#define Player2SecondPin 7
-#define Player1ButtonPin 11
-#define Player2ButtonPin 12
+// LED Pin locations
+#define Player1LED 7
+#define Player2LED 9
+#define MiddleLED 8
 
-int Player1Counter = 0, Player2Counter = 0;
+// Button Pin locations
+#define Player1Button 11
+#define Player2Button 12
  
 void setup() {
-  // configure LED pins to be digital outputs
-  pinMode(Player1FirstPin, OUTPUT);
-  pinMode(Player2FirstPin, OUTPUT);
-  pinMode(Player1SecondPin, OUTPUT);
-  pinMode(Player2SecondPin, OUTPUT);
-  pinMode(MiddlePin, OUTPUT);
+  // configure LED pins to be a digital outputs
+  pinMode(Player1LED, OUTPUT);
+  pinMode(Player2LED, OUTPUT);
+  pinMode(MiddleLED, OUTPUT);
  
   // set button pins to INPUT and 
   // turn on internal pull up resistor 
-  pinMode(Player1ButtonPin, INPUT);
-  digitalWrite(Player1ButtonPin, HIGH);
-  pinMode(Player2ButtonPin, INPUT);
-  digitalWrite(Player2ButtonPin, HIGH);
+  pinMode(Player1Button, INPUT);
+  digitalWrite(Player1Button, HIGH);
+  pinMode(Player2Button, INPUT);
+  digitalWrite(Player2Button, HIGH);
 }
-
-void Player1Pushed(){
-  if (Player2Counter == 0){
-    Player1Counter--;
-  }
-  else{
-    Player2Counter--;
-  }
-  LightUpLEDS(Player1FirstPin, -1, Player1Counter);
-}
-
-void Player2Pushed(){
-  if (Player1Counter == 0){
-    Player2Counter++;
-  }
-  else{
-    Player1Counter--;
-  }
-  LightUpLEDS(Player2FirstPin, 1, Player2Counter);
-  
-}
-
-void LightUpLEDS(int startLocation, int counter, int playerCounter){
-  for(int i = startLocation; i != startLocation + counter*3; i + counter){
-    
-  }
-  
-}
-
-
+// keep track of how many clicks are 
+// one player increases the count, other lowers it
+int PushCount = 0;
+// PushCount has to be threshold far away before LED changes
+int Threshold = 5;
  
 void loop() {
-  if ( digitalRead(Player1ButtonPin) == LOW ) {
-    Player1Pushed();
+  if ( digitalRead(Player1Button) == LOW ) {
+    PushCount++;
   }
-  if ( digitalRead(Player2ButtonPin) == LOW ) {
-    Player2Pushed();
+  if ( digitalRead(Player2Button) == LOW ) {
+    PushCount--;
   }
-  // wait 50 ms for any button bounce to die out
-  delay(50);
+ 
+  if ( PushCount > Threshold ) {
+    digitalWrite(Player1LED, HIGH);
+    digitalWrite(Player2LED, LOW);
+    digitalWrite(MiddleLED, LOW);
+  } else if ( PushCount < (0-Threshold) ) {
+    digitalWrite(Player1LED, LOW);
+    digitalWrite(Player2LED, HIGH);
+    digitalWrite(MiddleLED, LOW);
+  } else {
+    digitalWrite(Player1LED, LOW);
+    digitalWrite(Player2LED, LOW);
+    digitalWrite(MiddleLED, HIGH);
+  }
+ 
+  // wait 20 ms for any button bounce to die out
+  delay(20);
 }
